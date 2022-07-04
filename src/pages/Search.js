649e804ece4112eb59d2323d0ea28ef894400456
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-07-04 17:19:48 -0300

    concluido o requisito 5

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,16 +2,49 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      searchArtist: '',
+      minLength: true,
+    };
+  }
+
+  onInputChange = ({ target }) => {
+    const MIN_CHAR = 2;
+    this.setState(
+      {
+        searchArtist: target.value,
+        minLength: target.value.length < MIN_CHAR,
+      },
+    );
+  }
+
   render() {
+    const { minLength, searchArtist } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <main>
-          Conteúdo
-          <div>
-            <input type="text" placeholder="Nome do Artista" />
-            <button type="button">Procurar</button>
-          </div>
+          <form>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              placeholder="Nome do Artista"
+              value={ searchArtist }
+              onChange={ this.onInputChange }
+            />
+            <button
+              type="submit"
+              name="search"
+              disabled={ minLength }
+              data-testid="search-artist-button"
+              /* onClick={ this.onButtonClick } */
+            >
+              Pesquisar
+            </button>
+          </form>
         </main>
       </div>
     );
