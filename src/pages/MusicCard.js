e60d215ad8752a6920696e13d99d5f7a5b416036
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-07-05 19:09:32 -0300

    concluído os requisitos 9 e 11

--- src/pages/MusicCard.js
+++ src/pages/MusicCard.js
@@ -1,23 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import { addSong, removeSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
 
 class MusicCard extends React.Component {
   constructor() {
     super();
     this.state = {
       loading: false,
-      isChecked: false,
+      check: false,
+      /* favoriteList: [], */
     };
   }
+
+  /* componentDidMount() {
+    const { album } = this.props;
+    if (localStorage.getItem('favoriteSongs') === null) {
+      localStorage.setItem('favoriteSongs', JSON.stringify([]));
+    } else {
+      const favoriteList = JSON.parse(localStorage.getItem('favoriteSongs'));
+      this.setState({
+        isChecked: favoriteList.some((favorite) => favorite === album.trackId),
+      });
+    }
+  } */
+
+  async componentDidMount() {
+    const { album } = this.props;
+    this.setState({ loading: true });
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({
+      check: (
+        favoriteSongs.length > 0 ? favoriteSongs
+          .some((favorite) => favorite.trackId === album.trackId) : false),
+      loading: false,
+      /* favoriteList: favoriteSongs, */
+    });
+  }
+
+  /* saveLocalSotorage = (trackId) => {
+    const { isChecked } = this.state;
+    const favoriteList = JSON.parse(localStorage.getItem('favoriteSongs'));
+    if (isChecked) {
+      localStorage.setItem('favoriteSongs', JSON.stringify([...favoriteList, trackId]));
+    } else {
+      delete favoriteList[trackId];
+      localStorage.setItem('favoriteSongs', JSON.stringify([...favoriteList]));
+    }
+  } */
 
   onCheckFavorite = async ({ target }, album) => {
     this.setState({
       loading: true,
-      isChecked: target.checked });
+      check: target.checked });
     if (target.checked) {
+      /* this.saveLocalSotorage(album.trackId); */
       await addSong(album);
     } else {
+      /* this.saveLocalSotorage(album.trackId); */
       await removeSong(album);
     }
     this.setState({
@@ -26,7 +65,7 @@ class MusicCard extends React.Component {
   }
 
   render() {
-    const { loading, isChecked } = this.state;
+    const { loading, check } = this.state;
     const { album } = this.props;
     return (
       <div>
@@ -50,7 +89,7 @@ class MusicCard extends React.Component {
                 <input
                   type="checkbox"
                   data-testid={ `checkbox-music-${album.trackId}` }
-                  checked={ isChecked }
+                  checked={ check }
                   onChange={ (e) => this.onCheckFavorite(e, album) }
                 />
               </label>
