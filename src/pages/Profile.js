bcc8c717629ae6e1d3566ea1ed27e0d7d2f0b841
Marcell Rocha Pena <proartcarimbos@gmail.com.br>
2022-09-05 18:14:59 -0300

    finalizando secção de Perfil

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
 
-class Profile extends React.Component {
-  render() {
-    return (
-      <div data-testid="page-profile">
-        <Header />
-        <main>
-          Conteúdo
-        </main>
-      </div>
-    );
-  }
+function Profile() {
+  const [localData, setLocalData] = useState({});
+  const loadData = () => {
+    setLocalData(JSON.parse(localStorage.getItem('user')));
+  };
+
+  useEffect(() => {
+    loadData();
+  });
+
+  return (
+    <div data-testid="page-profile">
+      <Header />
+      {
+        Object.keys(localData).length === 0 ? <h1>Loading...</h1> : (
+          <main>
+            <img src={ localData.image } alt="profile-photography" />
+            <h1>{ localData.name }</h1>
+            <h1>{ localData.email }</h1>
+            <p>{ localData.description }</p>
+          </main>
+        )
+      }
+    </div>
+  );
 }
 
 export default Profile;
